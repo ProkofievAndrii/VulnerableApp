de4a11d4a0e3e4ff0f99b5a95b62eb3227226cb2
Prokofiev Andrii <prokofiev.main@gmail.com>
2026-02-26 23:45:14 +0100

    Added test project, ruleset, basic manual testing

---
+++ SecurityTestApp/SecurityTestApp/SecurityTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecurityTestAppApp.swift
+//  SecurityTestApp
+//
+//  Created by Andrii Prokofiev on 26.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SecurityTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
